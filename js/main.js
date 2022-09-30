fe6553f060739a3252b42630a5387ccf0d6cab3e
Alexander Peterikov <90040765+delosait-ru@users.noreply.github.com>
2022-09-30 12:15:06 +0300

    2
    2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,22 @@
-"use strict";
-
-console.log('global');
-"use strict";
-
 $(function () {
-  $('#slider-range-max').slider({
-    animate: 90,
-    range: 'max',
-    min: 1,
-    max: 100,
-    value: 10,
-    slide: function slide(event, ui) {
-      $('#percent').val(ui.value);
-    }
-  });
-  $('#slider-range-max2').slider({
-    animate: 90,
-    range: 'max',
-    min: 1,
-    max: 100,
-    value: 10,
-    slide: function slide(event, ui) {
-      $('#percent').val(ui.value);
-    }
-  });
+    $('#slider-range-max').slider({
+        animate: 90,
+        range: 'max',
+        min: 1,
+        max: 100,
+        value: 10,
+        slide: function (event, ui) {
+            $('#percent').val(ui.value);
+        }
+    });
+    $('#slider-range-max2').slider({
+        animate: 90,
+        range: 'max',
+        min: 1,
+        max: 100,
+        value: 10,
+        slide: function (event, ui) {
+            $('#percent').val(ui.value);
+        }
+    });
 });
-//# sourceMappingURL=main.js.map
